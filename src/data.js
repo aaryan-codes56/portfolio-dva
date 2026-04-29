414d6407caa5fd63768ca9e59b5cec6d5de9c5c5
Aaryan Krishna <aaryan.krishna2024@nst.rishihood.edu.in>
2026-04-29 22:48:01 +0530

    Update Cardiovascular Risk Assessment project details

--- src/data.js
+++ src/data.js
@@ -2,10 +2,10 @@ const base = import.meta.env.BASE_URL
 
 const projects = [
   {
-    name: "DVA Capstone",
-    description: "Data Visualization and Analytics Capstone Project.",
-    image: `${base}default.png`,
-    tags: ["Data Analytics"],
+    name: "Cardiovascular Risk Assessment",
+    description: "This project analyzes cardiovascular risk using patient health data to evaluate whether smoking and diabetes impact disease occurrence, heart rate, and cholesterol. It highlights single-factor correlations and emphasizes multivariate modeling, enabling data-driven healthcare decisions through interactive dashboards and analytical frameworks.",
+    image: "https://i.postimg.cc/FHvqPd8W/image.png",
+    tags: ["Python", "ML", "Data Analysis"],
     github: "https://github.com/RahulDwivedi205/G11_DVA_Capstone",
     live: "https://portfolio-website-mocha-xi-30.vercel.app/"
   }
